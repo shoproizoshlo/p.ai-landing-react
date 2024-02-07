36979cf78bdb503045cfc884a3d99cc906cbbce4
shoproizoshlo <iamsusunny@gmail.com>
2024-02-07 10:54:40 +0100

    Update Homepage.jsx

--- src/Components/Main/Homepage/Homepage.jsx
+++ src/Components/Main/Homepage/Homepage.jsx
@@ -10,7 +10,7 @@ const Homepage = () => {
         heading2="No targeting gaps."
         description="Use machine learning with first-party data to generate an endless amount of audiences for your marketing & advertising channels. No effort or delay & complementary to Customer Data Platforms."
       />
-      <Marketers />
+      {/* <Marketers /> */}
     </div>
   );
 };

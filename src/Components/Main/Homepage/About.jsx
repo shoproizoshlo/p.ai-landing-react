c40f675cab273dd4011c723cb343457fbd7ce197
shoproizoshlo <iamsusunny@gmail.com>
2024-02-08 18:18:55 +0100

    fixed some styling for about section

--- src/Components/Main/Homepage/About.jsx
+++ src/Components/Main/Homepage/About.jsx
@@ -14,21 +14,23 @@ const About = ({ headingAbout, imgAbout }) => {
         <img src={imgAbout} alt="" />
       </div>
       Components
-      <AboutSection
-        imgAboutSection={VolumeImg}
-        headingAboutSection="Volume"
-        descriptionAboutSection="Always-on, predictive acquisition and retention. Acquire new and repeat customers across any ad platform, at scale."
-      />
-      <AboutSection
-        imgAboutSection={EfficiencyImg}
-        headingAboutSection="Efficiency"
-        descriptionAboutSection="Predictive Audiences that ensure you invest the optimal amount based on every customer’s predicted conversion and customer lifetime value. No waste."
-      />
-      <AboutSection
-        imgAboutSection={UpliftImg}
-        headingAboutSection="Uplift"
-        descriptionAboutSection="Prove incrementality using multi-cell lift tests, with holdouts. This is real performance."
-      />
+      <div className="about-sections">
+        <AboutSection
+          imgAboutSection={VolumeImg}
+          headingAboutSection="Volume"
+          descriptionAboutSection="Always-on, predictive acquisition and retention. Acquire new and repeat customers across any ad platform, at scale."
+        />
+        <AboutSection
+          imgAboutSection={EfficiencyImg}
+          headingAboutSection="Efficiency"
+          descriptionAboutSection="Predictive Audiences that ensure you invest the optimal amount based on every customer’s predicted conversion and customer lifetime value. No waste."
+        />
+        <AboutSection
+          imgAboutSection={UpliftImg}
+          headingAboutSection="Uplift"
+          descriptionAboutSection="Prove incrementality using multi-cell lift tests, with holdouts. This is real performance."
+        />
+      </div>
       <p>We’d love to give you a demo.</p>
       <button>Book a demo</button>
     </div>

93f8c06e26318973e8ef1627862e67aad200aafc
shoproizoshlo <iamsusunny@gmail.com>
2024-02-18 12:40:21 +0100

    added classes Modal component

--- src/Components/Main/Modal.jsx
+++ src/Components/Main/Modal.jsx
@@ -4,11 +4,22 @@ import "../../styles/Modal.css";
 const Modal = ({ isOpen, onClose }) => {
   return (
     isOpen && (
-      <div className="modal-overlay" onClick={onClose}>
-        <div className="modal" onClick={(e) => e.stopPropagation()}>
+      <div
+        className="flex justify-center items-center fixed inset-0 w-full h-full z-30 modal-overlay"
+        onClick={onClose}
+      >
+        <div
+          className="p-8 bg-white rounded-xl text-center modal"
+          onClick={(e) => e.stopPropagation()}
+        >
           <p>Sorry!</p>
           <p>This is just a sample website.</p>
-          <button onClick={onClose}>Close</button>
+          <button
+            className="mt-3 pt-1 pb-1 ps-5 pe-4 bg-black text-white rounded-xl"
+            onClick={onClose}
+          >
+            Close
+          </button>
         </div>
       </div>
     )

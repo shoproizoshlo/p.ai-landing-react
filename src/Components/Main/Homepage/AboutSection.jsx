ccdd7f0f10534452717cdd7dd59992c0c51026ed
shoproizoshlo <iamsusunny@gmail.com>
2024-02-21 10:05:54 +0100

    added classes to about-section

--- src/Components/Main/Homepage/AboutSection.jsx
+++ src/Components/Main/Homepage/AboutSection.jsx
@@ -7,7 +7,7 @@ const AboutSection = ({
   openModal,
 }) => {
   return (
-    <div className="p-2 md:p-3 max-w-sm w-full md:w-1/3 flex flex-col items-center rounded about-section">
+    <div className="p-2 md:p-3 max-w-sm w-full md:w-1/3 flex flex-col items-center rounded bg-[#242424] about-section">
       <div className="h-2 rounded z-2 line"></div>
       <div className="m-5 sm:my-12 sm:mx-6 h-full flex flex-col items-start justify-between content-about-section">
         <div>
@@ -15,11 +15,15 @@ const AboutSection = ({
             <div>
               <img src={imgAboutSection} alt="" />
             </div>
-            <h4>{headingAboutSection}</h4>
+            <h4 className="ms-4 sm:ms-5 text-white text-xl">
+              {headingAboutSection}
+            </h4>
           </div>
-          <p>{descriptionAboutSection}</p>
+          <p className="mt-0 text-white">{descriptionAboutSection}</p>
         </div>
-        <a onClick={openModal}>View customer stories</a>
+        <a onClick={openModal} className="mt-0 text-white">
+          View customer stories
+        </a>
       </div>
     </div>
   );

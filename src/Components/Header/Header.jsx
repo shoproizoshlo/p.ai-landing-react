4a2f061b0aaeb0549385dea68161f18b1fcfe983
shoproizoshlo <iamsusunny@gmail.com>
2024-02-21 14:48:16 +0100

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import NavDesktop from "./NavDesktop";
 import NavMobile from "./NavMobile";
 import Logo from "../../assets/Logo.svg";
 
 const Header = ({ openModal }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <header className="mb-0 mt-0 ms-auto me-auto pt-5 pb-5 ps-4 pe-4 sm:p-12 lg:pt-10 lg:pb-10 lg:ps-4 lg:pe-4 fixed lg:relative flex flex-row items-center justify-between max-w-7xl w-full bg-white z-10">
-      <div>
+      <div onClick={() => navigate("/")} className="hover:cursor-pointer">
         <img src={Logo} alt="Logo" />
       </div>
       <NavMobile

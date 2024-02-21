b828d83cb239d54722f326d23a22a0ecf9710dbf
shoproizoshlo <iamsusunny@gmail.com>
2024-02-21 11:36:48 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 import Header from "./Components/Header/Header";
@@ -16,40 +17,27 @@ function App() {
 
   const openModal = () => {
     setIsModalOpen(true);
-    navigate("/");
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
+    navigate("/");
   };
   return (
     <>
       <Header openModal={openModal} />
       <Routes>
+        <Route path="/product" element={<Modal />} />
+        <Route path="/customers" element={<Modal />} />
+        <Route path="/about" element={<Modal />} />
+        <Route path="/blog" element={<Modal />} />
+        <Route path="/login" element={<Modal />} />
+        <Route path="/success-stories" element={<Navigate replace to="/" />} />
         <Route path="/" element={<Homepage />} />
-        <Route
-          path="/product"
-          element={<Modal isOpen={isModalOpen} onClose={closeModal} />}
-        />
-        <Route
-          path="/customers"
-          element={<Modal isOpen={isModalOpen} onClose={closeModal} />}
-        />
-        <Route
-          path="/about"
-          element={<Modal isOpen={isModalOpen} onClose={closeModal} />}
-        />
-        <Route
-          path="/blog"
-          element={<Modal isOpen={isModalOpen} onClose={closeModal} />}
-        />
-        <Route
-          path="/login"
-          element={<Modal isOpen={isModalOpen} onClose={closeModal} />}
-        />
       </Routes>
 
       <Main openModal={openModal} />
+
       <Footer openModal={openModal} />
 
       <Modal isOpen={isModalOpen} onClose={closeModal} />

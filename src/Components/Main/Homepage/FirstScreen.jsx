03a18d3bda61f3de86b5f8ecc94e403474e778e9
shoproizoshlo <iamsusunny@gmail.com>
2024-02-21 10:15:10 +0100

    added classes to FirstScreen

--- src/Components/Main/Homepage/FirstScreen.jsx
+++ src/Components/Main/Homepage/FirstScreen.jsx
@@ -3,9 +3,9 @@ import "../../../styles/FirstScreen.css";
 const FirstScreen = ({ heading, heading2, description }) => {
   return (
     <div className="max-w-7xl w-full mt-12 mb-28 sm:my-36 lg:mt-28 lg:mb-36 mx-auto py-0 px-5 first-screen">
-      <h1>{heading}</h1>
-      <h2>{heading2}</h2>
-      <p>{description}</p>
+      <h1 className="my-3 mx-auto max-w-xl text-center text-3xl">{heading}</h1>
+      <h2 className="mb-12 text-[#7e5fff] text-center text-3xl">{heading2}</h2>
+      <p className="my-3 mx-auto text-center text-base">{description}</p>
     </div>
   );
 };

bcaf00f06d5e2db131109fc83dd3dc34bea5a047
shoproizoshlo <iamsusunny@gmail.com>
2024-02-21 09:42:08 +0100

    added classes to footer links

--- src/Components/Footer/FooterLinks.jsx
+++ src/Components/Footer/FooterLinks.jsx
@@ -3,8 +3,8 @@ import "../../styles/FooterLinks.css";
 const FooterLinks = ({ footerLinksTitle, children }) => {
   return (
     <div className="mb-4 md:mb-0 flex flex-col footer-links">
-      <h5>{footerLinksTitle}</h5>
-      <ul className="flex flex-col">{children}</ul>
+      <h5 className="mb-0 sm:mb-7 text-lg sm:text-2xl">{footerLinksTitle}</h5>
+      <ul className="flex flex-col text-sm tracking-widest">{children}</ul>
     </div>
   );
 };

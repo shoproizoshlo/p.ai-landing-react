5f2eca66c101f65b9cf135e4d54d285dcb67f99f
shoproizoshlo <iamsusunny@gmail.com>
2024-02-21 10:54:07 +0100

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
@@ -6,23 +7,42 @@ import Footer from "./Components/Footer/Footer";
 import Modal from "./Components/Main/Modal";
 
 import "./App.css";
+import Homepage from "./Components/Main/Homepage/Homepage";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  
   return (
     <>
-      <Header openModal={openModal} />
-      <Main openModal={openModal} closeModal={closeModal} />
-      <Footer openModal={openModal} closeModal={closeModal} />
-      <Modal isOpen={isModalOpen} onClose={closeModal} />
+      <Router>
+        <Header openModal={openModal} />
+
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route
+            path="/product"
+            element={<Modal isOpen={isModalOpen} onClose={closeModal} />}
+          />
+          <Route
+            path="/customers"
+            element={<Modal isOpen={isModalOpen} onClose={closeModal} />}
+          />
+          <Route
+            path="/about"
+            element={<Modal isOpen={isModalOpen} onClose={closeModal} />}
+          />
+          <Route
+            path="/blog"
+            element={<Modal isOpen={isModalOpen} onClose={closeModal} />}
+          />
+          <Route
+            path="/login"
+            element={<Modal isOpen={isModalOpen} onClose={closeModal} />}
+          />
+        </Routes>
+
+        <Main openModal={openModal} closeModal={closeModal} />
+        <Footer openModal={openModal} closeModal={closeModal} />
+      </Router>
     </>
   );
 }
